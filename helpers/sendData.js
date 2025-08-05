992929f26a2aeb7e4f423248205fd9cfe0d8296d
Artiom Suqiasyan <artiom.suqiasyan1401@gmail.com>
2025-08-05 23:42:28 +0400

    feat: update

--- helpers/sendData.js
+++ helpers/sendData.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
-const TOKEN =
-  "e8e81c19a8837c981236bf743b6178b9a567760592661c4789d2a464943213098dd9bb0b66ca41b02c34f6928e781a028a88c0d80e0c7acc3deab22a1ba0525b";
+const TOKEN = process.env.TOKEN;
 
 export default async function sendData({ url, payload }) {
   try {
--- index.js
+++ index.js
@@ -4,8 +4,7 @@ import { getThemesIds } from "./helpers/emis/getThemesIds.js";
 import { savePlans } from "./helpers/emis/savePlans.js";
 import { saveThemes } from "./helpers/emis/saveThemes.js";
 
-const ID =
-  "7d72f234bc7b391779984e1327fe1925131443b19bece046cbd1f1730b5a0c39bdd849";
+const ID = process.env.ID;
 
 const readDocxFile = async (filePath) => {
   const value = await getDataFromDocx(filePath);
